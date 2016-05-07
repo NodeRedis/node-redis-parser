1184bd68508c87a4dfafbebb2154c2a50c04f04d
Ruben Bridgewater <ruben.bridgewater@fintura.de>
2016-05-07 06:14:53 +0200

    Add big data to benchmark

--- benchmark/index.js
+++ benchmark/index.js
@@ -1,15 +1,33 @@
 var Benchmark = require('benchmark');
-var assert = require('assert');
-var suite = new Benchmark.Suite;
+var suite = new Benchmark.Suite();
 
-var Parser = require('./../lib/parser');
+var Parser = require('./../');
 var ParserOLD = require('./old/parser');
 
-function returnError(error) {
-	// throw error; silent for err error perf test
+function returnError (error) {
+	error = null;
 }
 
-function checkReply() {
+function checkReply () {}
+
+function shuffle (array) {
+	var currentIndex = array.length;
+	var temporaryValue;
+	var randomIndex;
+
+	// While there remain elements to shuffle...
+	while (currentIndex !== 0) {
+		// Pick a remaining element...
+		randomIndex = Math.floor(Math.random() * currentIndex);
+		currentIndex -= 1;
+
+		// And swap it with the current element.
+		temporaryValue = array[currentIndex];
+		array[currentIndex] = array[randomIndex];
+		array[randomIndex] = temporaryValue;
+	}
+
+	return array;
 }
 
 var startBuffer = new Buffer('$100\r\nabcdefghij');
@@ -19,6 +37,16 @@ var integerBuffer = new Buffer(':1237884\r\n');
 var errorBuffer = new Buffer('-Error ohnoesitbroke\r\n');
 var arrayBuffer = new Buffer('*1\r\n*1\r\n$1\r\na\r\n');
 var endBuffer = new Buffer('\r\n');
+var lorem = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, ' +
+	'sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ' +
+	'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ' +
+	'ut aliquip ex ea commodo consequat. Duis aute irure dolor in'; // 256 chars
+var bigStringArray = (new Array(Math.pow(2, 16) / lorem.length).join(lorem + ' ')).split(' '); // Math.pow(2, 16) chars long
+var startBigBuffer = new Buffer('$' + (4 * 1024 * 1024) + '\r\n' + lorem);
+var chunks = new Array(64);
+for (var i = 0; i < 64; i++) {
+	chunks[i] = new Buffer(shuffle(bigStringArray).join(' ') + '.'); // Math.pow(2, 16) chars long
+}
 
 var parserOld = new ParserOLD({
 	returnReply: checkReply,
@@ -64,6 +92,32 @@ suite.add('NEW CODE: multiple chunks in a bulk string', function () {
 	parser.execute(chunkBuffer);
 	parser.execute(chunkBuffer);
 	parser.execute(chunkBuffer);
+	parser.execute(endBuffer);
+});
+
+// BIG BULK STRING
+
+suite.add('\nOLD CODE: 4mb bulk string', function () {
+	parserOld.execute(startBigBuffer);
+	for (var i = 0; i < 64; i++) {
+		parserOld.execute(chunks[i]);
+	}
+	parserOld.execute(endBuffer);
+});
+
+suite.add('HIREDIS: 4mb bulk string', function () {
+	parserHiRedis.execute(startBigBuffer);
+	for (var i = 0; i < 64; i++) {
+		parserHiRedis.execute(chunks[i]);
+	}
+	parserHiRedis.execute(endBuffer);
+});
+
+suite.add('NEW CODE: 4mb bulk string', function () {
+	parser.execute(startBigBuffer);
+	for (var i = 0; i < 64; i++) {
+		parser.execute(chunks[i]);
+	}
 	parser.execute(endBuffer);
 });
 
@@ -134,5 +188,4 @@ suite.on('complete', function () {
 	console.log('\n\nFastest is ' + this.filter('fastest').map('name'));
 });
 
-
-suite.run({delay:2, minSamples: 100 });
+suite.run({ delay: 1, minSamples: 150 });
